a5990b7e0bacfe2ae3d077184f761369c3f2bc7c
xize <glucassen@live.nl>
2017-01-21 02:15:31 +0100

    initial release

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GTAV_cloud_remover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(249, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "clean temp folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 31);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(249, 23);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.UseWaitCursor = true;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(13, 12);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(38, 13);
+            this.status.TabIndex = 2;
+            this.status.Text = "status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 94);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GTA5 Cloud file remover";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GTAV_cloud_remover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = Path.GetTempPath();
+            IEnumerable<string> files;
+            try
+            {
+                files = Directory.EnumerateFiles(path, "cloud_*.dat", SearchOption.TopDirectoryOnly);
+            } catch(Exception)
+            {
+                MessageBox.Show("no files found", "error: no files!");
+                return;
+            }
+            int allfiles = files.ToArray().Length;
+
+            if(allfiles <= 0)
+            {
+                MessageBox.Show("no files found", "error: no files!");
+                return;
+            }
+
+            int index = 0;
+            long fileSize = 0;
+            while (files.GetEnumerator().MoveNext())
+            {
+                string file = files.First();
+                Console.WriteLine("removing file: "+file);
+                fileSize += new FileInfo(file).Length;
+                File.Delete(file);
+                try
+                {
+                    progressBar1.Value = (int)((100 * index++) / allfiles - 1);
+                } catch(Exception)
+                {
+                    Console.WriteLine("value: "+allfiles+","+(allfiles-1)+","+(100*index++));
+                }
+                 Console.WriteLine("index: "+index);
+            }
+
+            MessageBox.Show("in total "+allfiles+" files have been removed\n\nthat is in total: "+fileSize / 1048+"kb", "complete!");
+
+        }
+    }
+}
